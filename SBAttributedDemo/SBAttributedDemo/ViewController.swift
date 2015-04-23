94bfe69b44c790b44e8259a96adbb8a18a3c8090
ShinyaSakemoto <sakemoto@jxpress-d05.local>
2015-04-23 15:19:21 +0900

    Add demo project

---
+++ SBAttributedDemo/SBAttributedDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBAttributedDemo
+//
+//  Created by ShinyaSakemoto on 2015/04/23.
+//  Copyright (c) 2015年 sakebook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
