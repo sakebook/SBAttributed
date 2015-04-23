b50048b61f7916d12501ce243414bee8dde46b9f
ShinyaSakemoto <sakemoto@jxpress-d05.local>
2015-04-23 13:21:55 +0900

    Initial Commit

---
+++ SBAttributed/SBAttributed.h
@@ -0,0 +1,19 @@
+//
+//  SBAttributed.h
+//  SBAttributed
+//
+//  Created by ShinyaSakemoto on 2015/04/23.
+//  Copyright (c) 2015年 sakebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBAttributed.
+FOUNDATION_EXPORT double SBAttributedVersionNumber;
+
+//! Project version string for SBAttributed.
+FOUNDATION_EXPORT const unsigned char SBAttributedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBAttributed/PublicHeader.h>
+
+
