be0c0c03f0f3c2dcce7a0e540fb6116d2f562cde
ShinyaSakemoto <sakemoto@jxpress-d05.local>
2015-04-23 13:37:15 +0900

    Add fundation class

---
+++ SBAttributed/SBAttributed.swift
@@ -0,0 +1,12 @@
+//
+//  SBAttributed.swift
+//  SBAttributed
+//
+//  Created by ShinyaSakemoto on 2015/04/23.
+//  Copyright (c) 2015年 sakebook. All rights reserved.
+//
+
+import Foundation
+
+final class SBAttributed: NSObject {
+}
